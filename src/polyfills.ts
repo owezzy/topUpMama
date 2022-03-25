97a2aeef1ea80d200e73aab1a5261690b922ac77
owezzy <owenadira@gmail.com>
2022-03-25 23:05:16 +0300

    add pollyfils

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -59,3 +59,5 @@ import 'zone.js';  // Included with Angular CLI.
 (window as any).process = {
   env: { DEBUG: undefined },
 };
+(window as any).global.util = (window as any).global.util || require('util').util;
+
