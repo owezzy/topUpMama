a5a26b5f93410d5595743353cc3c579c7c188340
owezzy <owenadira@gmail.com>
2022-04-03 02:02:40 +0300

    fix app init function

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -1,8 +1,8 @@
 import {AuthService} from "../services/auth.service";
 import {User} from "../users/model/user";
 
-export function appInitializer(authenticationService: AuthService) {
-    return () => new Promise(resolve => {
+export function appInitializer(authenticationService: AuthService): Promise<any> {
+    return new Promise(resolve => {
         // attempt to refresh token on app start up to auto authenticate
         // collect user data from localstorage if available
       // @ts-ignore
